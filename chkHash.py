957ad9d531a780e665f3486f3e570f362db46635
suzumiya3 <133917467+suzumiya-3@users.noreply.github.com>
2023-06-07 22:11:44 +0900

    Update chkHash.py

--- chkHash.py
+++ chkHash.py
@@ -1,15 +1,29 @@
 import hashlib
 
+def read_chk(type):
+    for chunk in iter(lambda: f.read(2048 * type.block_size), b''):
+            type.update(chunk)
+    return type
+
 while True:
     files = input('Please select file: ')
+    if files == "":
+        continue
 
     for file in files.split(','):
         with open(file, 'rb') as f:
-            data = f.read()
 
-            md5 = hashlib.md5(data).hexdigest()
-            sha1 = hashlib.sha1(data).hexdigest()
-            sha256 = hashlib.sha256(data).hexdigest()
-            sha512 = hashlib.sha512(data).hexdigest()
+            # md5
+            md5 = hashlib.md5()
+            hash_md5 = read_chk(md5).hexdigest()
+            # sha1
+            sha1 = hashlib.sha1()
+            hash_sha1 = read_chk(sha1).hexdigest()
+            # sha265
+            sha256 = hashlib.sha256()
+            hash_sha256 = read_chk(sha256).hexdigest()
+            # sha512
+            sha512 = hashlib.sha512()
+            hash_sha512 = read_chk(sha512).hexdigest()
 
-            print(f'md5: {md5}\nsha1: {ha1}\nsha256: {sha256}\nsha512: {sha512}')
+            print(f'md5: {hash_md5}\nsha1: {hash_sha1}\nsha256: {hash_sha256}\nsha512: {hash_sha512}\n')
